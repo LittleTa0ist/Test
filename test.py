fbcea7ac4ea13f644d0eb36a0b210135282ea1b6
pzx <pzx@bit.edu.cn>
2022-04-28 16:28:36 +0800

    第一次提交

--- test.py
+++ test.py
@@ -1,6 +1,1 @@
-from fuzzy_extractor import FuzzyExtractor
-extractor = FuzzyExtractor(16, 8)
-key, helper = extractor.generate('AABBCCDDEEFFGGHH')
-r_key = extractor.reproduce('AABBCCDDEEFFGGHH', helper)  # r_key should equal key
-r_key = extractor.reproduce('AABBCCDDEEFFGGHI', helper)  # r_key will probably still equal key!
-r_key = extractor.reproduce('AAAAAAAAAAAAAAAA', helper)  # r_key is no longer likely to equal key
+print(11111)
