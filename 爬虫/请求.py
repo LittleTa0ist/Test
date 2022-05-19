245d50dd78d9df5b89d537de589f7d3a4054e9fc
pzx <pzx@bit.edu.cn>
2022-05-19 11:11:15 +0800

    爬虫学习

--- 爬虫/请求.py
+++ 爬虫/请求.py
@@ -1,12 +1,25 @@
 import requests
 
-url='https://www.sogou.com/web?query=周杰伦'
-resp=requests.get(url)
-print(resp.text)
+# name=input("请输入要查询的人名：")
+# par={'query':'周杰伦'}
+# url='https://www.sogou.com/web?'
+# headers={
+#     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36'
+# }
+# resp =requests.get(url,headers=headers,params=par)
+# print(resp.text)
 
+url="https://movie.douban.com/j/chart/top_list?"
 headers={
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36'
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36"
 }
-resp =requests.get(url,headers=headers)
-with open('jay.html',mode='w',encoding='utf-8') as f:
-    f.write(resp.text)
+par={
+    "type": "24",
+    "interval_id":"100:90",
+    "action":"",
+    "start": "0",
+    "limit": "20"
+ }
+resp=requests.get(url,params=par,headers=headers)
+print(resp.json())
+resp.close()
